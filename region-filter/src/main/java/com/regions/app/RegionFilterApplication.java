727b44c51f0dbbaf4ab9f7a3a9910924065d33dd
Git2018T <arun.s.blore@gmail.com>
2023-04-08 03:22:30 +0200

    Initial commit

---
+++ region-filter/src/main/java/com/regions/app/RegionFilterApplication.java
@@ -0,0 +1,13 @@
+package com.regions.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegionFilterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegionFilterApplication.class, args);
+	}
+
+}
---
+++ region-filter/src/test/java/com/regions/app/RegionFilterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regions.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegionFilterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
